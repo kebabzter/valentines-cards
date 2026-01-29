8f0d2227a147ae75b5cb03e1af2a8b478e76d2de
kebabzter <gabzterthesecond@gmail.com>
2026-01-29 15:37:01 +0100

    Changed font to jetbrains mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata, Viewport } from "next";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { HeartsBackground } from "@/components/HeartsBackground";
+
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Valentine's Cards",
@@ -22,20 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font -- Fonts via link (next/font not working in env) */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="antialiased bg-hearts">
+      <body
+        className={`${jetbrainsMono.variable} antialiased bg-hearts`}
+      >
         <HeartsBackground />
         {children}
       </body>
